0260583e2e0733fef3182edd9de0c698258a2b01
Rohan1192 <81288264+Rohan1192@users.noreply.github.com>
2024-05-17 10:22:28 +0530

    help1

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h2>Hello World!</h2>\
+     <h2>Hello World!</h2>
      <h2>ROHAN REDDY</h2>
+     <h2>WELCOME</h2>
     </div>
   );
 }
